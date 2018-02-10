df8b432d11c85f696c977c0f80c0ab9301ffad38
Devin Alvaro <devin.alvaro@gmail.com>
2018-02-10 19:14:30 +0700

    Add minimum viable /user/login

--- web/app.py
+++ web/app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+
+@app.route('/user/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    return username + ' ' + password
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
