729a8632103273459180901dc24a7914093e906e
Devin Alvaro <devin.alvaro@gmail.com>
2018-02-10 15:38:22 +0700

    Remove welcome message

--- web/app.py
+++ web/app.py
@@ -1,14 +1,6 @@
 from flask import Flask
 
-import src.undepress
-
 app = Flask(__name__)
-
-
-@app.route('/')
-def welcome():
-    return src.undepress.welcome()
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
