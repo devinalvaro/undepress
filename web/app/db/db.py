391b8ff0d7cd433c5ad409e54a9fba2e398f8aab
Devin Alvaro <devin.alvaro@gmail.com>
2018-02-19 22:06:36 +0700

    Update docker-compose.yml to version '3'

--- web/app/db/db.py
+++ web/app/db/db.py
@@ -3,5 +3,5 @@ from pymongo import MongoClient
 
 
 class Db:
-    __mongo_client = MongoClient(environ['DB_PORT_27017_TCP_ADDR'], 27017)
+    __mongo_client = MongoClient('db', 27017)
     _db = __mongo_client.undepress
