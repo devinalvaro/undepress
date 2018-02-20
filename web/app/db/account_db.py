0bfdfb408cf5e54a0919a1a84b200bbc0bb0c203
Devin Alvaro <devin.alvaro@gmail.com>
2018-02-20 15:07:18 +0700

    Rethink Db class

--- web/app/db/account_db.py
+++ web/app/db/account_db.py
@@ -2,17 +2,8 @@ from .db import Db
 
 
 class AccountDb(Db):
-    __accounts = Db._db.accounts
-
-    def find(self, **query):
-        return self.__accounts.find(query)
-
-    def find_one(self, **query):
-        return self.__accounts.find_one(query)
+    _collection = Db._db.accounts
 
     def insert(self, **query):
         query['user_id'] = self.get_size() + 1
-        self.__accounts.insert(query)
-
-    def get_size(self):
-        return self.find().count()
+        Db.insert(query)
--- web/app/db/db.py
+++ web/app/db/db.py
@@ -1,7 +1,19 @@
-from os import environ
 from pymongo import MongoClient
 
 
 class Db:
     __mongo_client = MongoClient('db', 27017)
     _db = __mongo_client.undepress
+    _collection = None
+
+    def find(self, **query):
+        return self._collection.find(query)
+
+    def find_one(self, **query):
+        return self._collection.find_one(query)
+
+    def insert(self, **query):
+        self._collection.insert(query)
+
+    def get_size(self):
+        return self.find().count()
