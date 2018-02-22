dec22462de0eb45728efcc677e7ec07eea3985aa
Devin Alvaro <devin.alvaro@gmail.com>
2018-02-22 15:47:59 +0700

    Readd Db.find_one() where it's necessary

--- web/app/db/db.py
+++ web/app/db/db.py
@@ -12,6 +12,9 @@ class Db:
     def find(self, **query):
         return self._collection.find(query)
 
+    def find_one(self, **query):
+        return self._collection.find_one(query)
+
     def insert(self, **query):
         self._collection.insert(query)
 
