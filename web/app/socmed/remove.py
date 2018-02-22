844786091356e3f898917e59adbb70c50f7d0f5b
Devin Alvaro <devin.alvaro@gmail.com>
2018-02-22 15:48:52 +0700

    Add @login_required to socmed/remove.py

--- web/app/socmed/remove.py
+++ web/app/socmed/remove.py
@@ -1,10 +1,12 @@
 from flask import request
+from flask_login import login_required
 
 from . import socmed
 from ..db import SocmedDb
 
 
 @socmed.route('/remove', methods=['POST'])
+@login_required
 def remove():
     user_id = int(request.form['user_id'])
     twitter = request.form.get('twitter')
