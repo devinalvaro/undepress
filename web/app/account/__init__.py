c8fbfc34d9e1672bf110f1eeeaa3f30cc1ebab4b
Devin Alvaro <devin.alvaro@gmail.com>
2018-02-22 16:17:57 +0700

    Fix an import bug

--- web/app/account/__init__.py
+++ web/app/account/__init__.py
@@ -7,8 +7,9 @@ from ..db import AccountDb
 account = Blueprint('account', __name__, url_prefix='/account')
 
 from .login import login
-from .register import register
 from .logout import logout
+from .register import register
+from .user import User
 
 
 @account.route('/', methods=['GET'])
