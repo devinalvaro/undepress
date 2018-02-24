b7614d8afc791a2e419fd008bff844d89026f760
Devin Alvaro <devin.alvaro@gmail.com>
2018-02-24 21:17:03 +0700

    Update api/__init__.py

--- web/app/api/__init__.py
+++ web/app/api/__init__.py
@@ -1,4 +1,7 @@
 from .account import account as account_blueprint
+from .appointment import appointment as appointment_blueprint
 from .chat import chat as chat_blueprint
+from .monitoring import monitoring as monitoring_blueprint
 from .socmed import socmed as socmed_blueprint
+from .trainingdata import trainingdata as trainingdata_blueprint
 from .userdata import userdata as userdata_blueprint
