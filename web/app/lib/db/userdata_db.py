70a1d1e94c9795453c519db03642e085cfc5b90c
Devin Alvaro <devin.alvaro@gmail.com>
2018-02-24 15:18:27 +0700

    Drop data_id

--- web/app/lib/db/userdata_db.py
+++ web/app/lib/db/userdata_db.py
@@ -3,8 +3,3 @@ from .db import Db
 
 class UserdataDb(Db):
     Db._collection = Db._db.userdata
-
-    @staticmethod
-    def insert(**query):
-        Db.put_id(query, 'data_id')
-        Db.insert(**query)
