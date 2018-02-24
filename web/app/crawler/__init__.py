819fe2debbb007769c765723d4f3a0037d9a45b9
Devin Alvaro <devin.alvaro@gmail.com>
2018-02-24 13:39:38 +0700

    Drop crawler API

--- web/app/crawler/__init__.py
+++ web/app/crawler/__init__.py
@@ -1,7 +1,1 @@
-from flask import Blueprint, current_app as app, request
-
 from .twitter_api import TwitterAPI
-
-crawler = Blueprint('crawler', __name__, url_prefix='/crawler')
-
-from .twitter import fetch
--- web/app/crawler/twitter_api.py
+++ web/app/crawler/twitter_api.py
@@ -1,4 +1,7 @@
+from flask_login import current_user
 from twitter import Twitter, OAuth
+
+from ..db import SocmedDb
 
 
 class TwitterAPI:
@@ -10,6 +13,11 @@ class TwitterAPI:
         self.__oauth = OAuth(self.__ACCESS_TOKEN, self.__ACCESS_SECRET,
                              self.__CONSUMER_KEY, self.__CONSUMER_SECRET)
         self.__twitter = Twitter(auth=self.__oauth)
+
+    def fetch_current_user_timeline(self):
+        user_id = current_user.user_id
+        username = SocmedDb.find_one(user_id=user_id)['twitter']
+        return self.fetch_user_timeline(username)
 
     def fetch_user_timeline(self, username):
         return self.__twitter.statuses.user_timeline(screen_name=username)
