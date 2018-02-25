2711b58da5316198fda58593f578d38b3d1f61f9
Devin Alvaro <devin.alvaro@gmail.com>
2018-02-25 15:51:31 +0700

    Fix more import bugs

--- web/app/lib/token/__init__.py
+++ web/app/lib/token/__init__.py
@@ -1,1 +1,3 @@
-from .token import generate_token, set_token, unset_token
+from .token import generate_token, parse_token
+from .token import is_token_valid
+from .token import set_token, unset_token
