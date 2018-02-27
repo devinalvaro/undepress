a3afc95ecbe7acd1aff296d66e03900f943202b1
Devin Alvaro <devin.alvaro@gmail.com>
2018-02-27 21:21:39 +0700

    Add ensure_boolean() and ensure_hashed()

--- web/app/api/account/set.py
+++ web/app/api/account/set.py
@@ -3,6 +3,7 @@ from flask_login import current_user, login_required
 
 from . import account
 from ...lib.db import AccountDb
+from ...lib.password import hash_password
 
 
 @account.route('/set', methods=['POST'])
@@ -17,12 +18,13 @@ def set():
 def get_form_data(request):
     return dict(
         email=request.form.get('email'),
-        password=request.form.get('password'),
+        password=ensure_hashed(request.form.get('password')),
         name=request.form.get('name'),
         address=request.form.get('address'),
         phone=request.form.get('phone'),
-        is_expert=bool(request.form.get('is_expert')),
-        is_expert_verified=bool(request.form.get('is_expert_verified')),
+        is_expert=ensure_boolean(request.form.get('is_expert')),
+        is_expert_verified=ensure_boolean(
+            request.form.get('is_expert_verified')),
         expert_description=request.form.get('expert_description'),
         expert_experience=request.form.get('expert_experience'),
         expert_location=request.form.get('expert_location'),
@@ -31,3 +33,11 @@ def get_form_data(request):
 
 def set_account(form_data):
     AccountDb.set(dict(user_id=current_user.user_id), **form_data)
+
+
+def ensure_hashed(password):
+    return hash_password(password) if password is not None else None
+
+
+def ensure_boolean(boolean):
+    return bool(boolean) if boolean is not None else None
