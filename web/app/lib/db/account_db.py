683f53b18e0909bf4474e329926251f6155cb4e6
Devin Alvaro <devin.alvaro@gmail.com>
2018-02-28 18:58:10 +0700

    Exclude password from /account

--- web/app/lib/db/account_db.py
+++ web/app/lib/db/account_db.py
@@ -5,6 +5,16 @@ class AccountDb(Db):
     collection = Db._db.accounts
 
     @classmethod
+    def find(cls, **query):
+        exclude = dict(password=0)
+        return cls.collection.find(query, exclude)
+
+    @classmethod
+    def find_one(cls, **query):
+        exclude = dict(password=0)
+        return cls.collection.find_one(query, exclude)
+
+    @classmethod
     def insert(cls, **query):
         cls._put_id(query, 'user_id')
         super(AccountDb, cls).insert(**query)
