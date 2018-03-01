8090397e242445e7981139de20f250dc08dc3437
Devin Alvaro <devin.alvaro@gmail.com>
2018-03-01 16:03:14 +0700

    Add sum_predictions()

--- web/app/api/detection/__init__.py
+++ web/app/api/detection/__init__.py
@@ -11,17 +11,18 @@ detection = Blueprint('detection', __name__, url_prefix='/detection')
 @detection.route('', methods=['GET'])
 @login_required
 def index():
-    return json_util.dumps(get_depression_detection(app))
+    depression_detection = detect_depression(app)
+    return json_util.dumps(depression_detection)
 
 
-def get_depression_detection(app):
+def detect_depression(app):
     twitter_crawler = TwitterCrawler(app)
     tweets = twitter_crawler.fetch_current_user_timeline()
 
     predictions = [
         detect_depression_symptoms(tweet['text']) for tweet in tweets
     ]
-    return predictions
+    return sum_predictions(predictions)
 
 
 def detect_depression_symptoms(text):
@@ -30,3 +31,13 @@ def detect_depression_symptoms(text):
         classifier = joblib.load('data/classifier_' + str(i) + '.pkl')
         prediction.extend(classifier.predict([text]))
     return prediction
+
+
+def sum_predictions(predictions):
+    predictions_sum = [0 for _ in range(9)]
+    for prediction in predictions:
+        if int(prediction[0]):
+            predictions_sum = [
+                a + int(b) for a, b in zip(predictions_sum, prediction)
+            ]
+    return predictions_sum
